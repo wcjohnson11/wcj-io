40e236d05d71c19d5f19ed7845fb2aed9e2704b7
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-06 14:03:47 -0500

    ad typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography";
+import USWebStandards from "typography-theme-us-web-design-standards";
+
+const typography = new Typography(USWebStandards);
+
+export default typography;
