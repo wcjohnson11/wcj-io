caebadf42de3b024f2a01c188ffb0fe6d9bbf090
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-07 10:09:11 -0500

    destructure allMarkdownRemark

--- src/components/archive.js
+++ src/components/archive.js
@@ -6,7 +6,7 @@
  */
 
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { graphql, Link, StaticQuery } from "gatsby"
 
 const POST_ARCHIVE_QUERY = graphql`
     query BlogArchiveQuery {
@@ -27,14 +27,14 @@ const POST_ARCHIVE_QUERY = graphql`
 const Archive = () => (
   <StaticQuery
     query={POST_ARCHIVE_QUERY}
-    render={allMarkdownRemark => (
+    render={({allMarkdownRemark}) => (
       <>
        <aside>
             <h3>Archive</h3>
             <ul>
                 {allMarkdownRemark.edges.map(edge => (
                     <li key={edge.node.frontmatter.slug}>
-                        {edge.node.frontmatter.title}
+                        <Link to={`/posts/${edge.node.frontmatter.slug}`}>{edge.node.frontmatter.title}</Link>
                     </li>
                 ))}
             </ul>
