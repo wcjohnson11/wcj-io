cbf2a579f8009d120a86d369657b2ece85777b00
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-07 10:04:50 -0500

    Create blog page and archive component

---
+++ src/components/archive.js
@@ -0,0 +1,47 @@
+/**
+ * Archive component that queries for blog posts
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+const POST_ARCHIVE_QUERY = graphql`
+    query BlogArchiveQuery {
+        allMarkdownRemark {
+            edges {
+                node {
+                    excerpt
+                    frontmatter {
+                        title
+                        slug
+                    }
+                }
+            }
+        }
+    }
+`
+
+const Archive = () => (
+  <StaticQuery
+    query={POST_ARCHIVE_QUERY}
+    render={allMarkdownRemark => (
+      <>
+       <aside>
+            <h3>Archive</h3>
+            <ul>
+                {allMarkdownRemark.edges.map(edge => (
+                    <li key={edge.node.frontmatter.slug}>
+                        {edge.node.frontmatter.title}
+                    </li>
+                ))}
+            </ul>
+       </aside>
+      </>
+    )}
+  />
+)
+
+export default Archive
---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Blog = () => (
+    <Layout>
+        <div>
+            <h1>Blog</h1>
+        </div>
+    </Layout>
+)
+
+export default Blog
