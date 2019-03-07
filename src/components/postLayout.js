fd3243f3a1e8bc6da6c3ab44ced1b26cd5fcfa5b
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-07 10:26:31 -0500

    create post layout component

---
+++ src/components/postLayout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Layout from './layout'
+
+export default class postLayout extends Component {
+    render() {
+        return (
+            <Layout>
+                <div>
+                    <h1>Post Layout</h1>
+                </div>
+            </Layout>
+        )
+    }
+}
