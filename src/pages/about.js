ff7abc3c1ad1242e19bd7f1bc269354c1fdc3b7e
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-07 00:38:15 -0500

    create query in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { graphql } from 'gatsby';
 import Layout from '../components/layout'
 
-const About = () => (
+const About = ({data}) => {
+    const {node} = data.allMarkdownRemark.edges[0]
+    const createMarkup = () => ({__html: node.html})
+    return (
     <Layout>
         <div>
             <h1>About</h1>
+            <h3>{node.frontmatter.title}</h3>
+            <div dangerouslySetInnerHTML={createMarkup()}/>
         </div>
     </Layout>
-)
+)}
 
 export default About
+
+export const query = graphql`
+    {
+        allMarkdownRemark {
+            edges {
+                node {
+                    html
+                    frontmatter {
+                        title
+                    }
+                }
+            }
+        }
+    }
+`
