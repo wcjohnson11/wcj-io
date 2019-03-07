7197ece7e703a614125ac8175db9e0da6c5135ef
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-07 10:05:12 -0500

    add archive to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Archive from '../components/archive'
 import Image from "../components/image"
 import SEO from "../components/seo"
 
@@ -14,6 +15,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Archive />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
