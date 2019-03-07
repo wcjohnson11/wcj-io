0c3a235818d192032c665a1de71f2b92813fef08
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-07 11:01:21 -0500

    create PostQuery in postLayout

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
+import { graphql } from 'gatsby'
 import Layout from './layout'
 
 export default class postLayout extends Component {
     render() {
+        const { markdownRemark } = this.props.data;
         return (
             <Layout>
                 <div>
-                    <h1>Post Layout</h1>
+                    <h1>{markdownRemark.frontmatter.title}</h1>
+                    <div dangerouslySetInnerHTML={{__html: markdownRemark.html}}/>
                 </div>
             </Layout>
         )
     }
 }
+
+export const query = graphql`
+    query PostQuery($slug: String!) {
+        markdownRemark(frontmatter: {
+            slug: { eq: $slug }
+        }) {
+            html
+            frontmatter {
+                title
+                date
+            }
+        }
+    }
+`
