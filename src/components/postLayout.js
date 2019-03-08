7a0e3fea6f406a30682bae3acacb8ab0411e8d30
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-08 11:06:54 -0500

    create blog post listing component

--- src/components/postLayout.js
+++ src/components/postLayout.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react'
-import { graphql } from 'gatsby'
-import Layout from './layout'
+import React, { Component } from 'react';
+import { graphql } from 'gatsby';
+import Layout from './layout';
 
 export default class postLayout extends Component {
-    render() {
-        const { markdownRemark } = this.props.data;
-        return (
-            <Layout>
-                <div>
-                    <h1>{markdownRemark.frontmatter.title}</h1>
-                    <div dangerouslySetInnerHTML={{
-                        __html: markdownRemark.html
-                    }}/>
-                </div>
-            </Layout>
-        )
-    }
+	render() {
+		const { markdownRemark } = this.props.data;
+		return (
+			<Layout>
+				<div>
+					<h1>{markdownRemark.frontmatter.title}</h1>
+					<div
+						dangerouslySetInnerHTML={{
+							__html : markdownRemark.html
+						}}
+					/>
+				</div>
+			</Layout>
+		);
+	}
 }
 
 export const query = graphql`
-    query PostQuery($slug: String!) {
-        markdownRemark(frontmatter: {
-            slug: { eq: $slug }
-        }) {
-            html
-            frontmatter {
-                title
-                date
-            }
-        }
-    }
-`
+	query PostQuery($slug: String!) {
+		markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+			html
+			frontmatter {
+				title
+				date
+			}
+		}
+	}
+`;
