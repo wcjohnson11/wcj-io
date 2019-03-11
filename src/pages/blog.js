6680b7a7ba5791180ecb8ee2639954e791bba2d0
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-11 17:20:52 -0400

    add location to pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-const Blog = () => (
-    <Layout>
+const Blog = ({location}) => (
+    <Layout location={location}>
         <div>
             <h1>Blog</h1>
         </div>
