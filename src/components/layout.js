4945f6a4c9a457f37fc53c346abecb900b2fe653
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-11 16:17:01 -0400

    add react-spring

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Img from "gatsby-image"
+import { Spring } from "react-spring/renderprops"
 import { StaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 
@@ -18,7 +19,7 @@ import "./layout.css"
 
 const MainLayout = styled.main`
   max-width: 90%;
-  margin: 0 auto;
+  margin: 1rem auto;
   display: grid;
   grid-template-columns: 3fr 1fr;
   grid-gap: 40px;
@@ -46,9 +47,16 @@ const Layout = ({ children, location }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        { location.pathname === "/" &&
-          <Img fluid={data.file.childImageSharp.fluid} />
-        }
+        <Spring
+          from={{height: location.pathname === "/" ? 100 : 200}}
+          to={{height: location.pathname === "/" ? 200 : 100}}
+        >
+          {styles => (
+            <div style={{overflow: 'hidden', ...styles}}>
+              <Img fluid={data.file.childImageSharp.fluid} />
+            </div>
+          )}
+        </Spring>
         <MainLayout>
           <div>
             {children}
