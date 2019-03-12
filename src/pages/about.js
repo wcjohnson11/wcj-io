7556c16182c4c3db4489526c513540d61abdd5db
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-12 15:23:53 -0700

    add image banner to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby';
+import { Spring } from "react-spring/renderprops"
+import Img from "gatsby-image"
 import Layout from '../components/layout'
 
 const About = ({data, location}) => {
@@ -8,6 +10,16 @@ const About = ({data, location}) => {
     return (
     <Layout location={location}>
         <div>
+            <Spring
+                from={{height: location.pathname === "/" ? 100 : 500}}
+                to={{height: location.pathname === "/" ? 500 : 100}}
+            >
+                {styles => (
+                <div style={{overflow: 'hidden', ...styles}}>
+                    <Img fluid={data.file.childImageSharp.fluid} />
+                </div>
+                )}
+            </Spring>
             <h1>About</h1>
             <h3>{node.frontmatter.title}</h3>
             <div dangerouslySetInnerHTML={createMarkup()}/>
@@ -28,6 +40,13 @@ export const query = graphql`
                     }
                 }
             }
+        }
+        file(relativePath: { regex: "/bg/"}) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
         }
     }
 `
