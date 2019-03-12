ba75ba79c83f70d7c388c30a7c3a8cac86d56a8c
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-11 23:32:54 -0400

    add gatsby remark images plugin and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,54 +1,81 @@
 module.exports = {
-  siteMetadata: {
-    title: `William Johnson Personal Site`,
-    description: `Analytics, Art, Data, Engineering`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://wcj.io`,
-    image: `/src/images/favicon.ico`
-  },
-  plugins: [
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+	siteMetadata : {
+		title       : `William Johnson Personal Site`,
+		description : `Analytics, Art, Data, Engineering`,
+		author      : `@gatsbyjs`,
+		siteUrl     : `https://wcj.io`,
+		image       : `/src/images/favicon.ico`
+	},
+	plugins      : [
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `William Johnson Personal Site`,
-        short_name: `wcj.io`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-        {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts`,
-      },
-    },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-netlify`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-netlify-cms`
-  ],
-}
+			resolve : `gatsby-source-filesystem`,
+			options : {
+				name : `uploads`,
+				path : `${__dirname}/static/img`
+			}
+		},
+		{
+			resolve : `gatsby-source-filesystem`,
+			options : {
+				name : `images`,
+				path : `${__dirname}/src/images`
+			}
+		},
+		{
+			resolve : `gatsby-source-filesystem`,
+			options : {
+				name : `posts`,
+				path : `${__dirname}/src/posts`
+			}
+		},
+		{
+			resolve : `gatsby-transformer-remark`,
+			options : {
+				plugins : [
+					// gatsby-remark-relative-images must
+					// go before gatsby-remark-images
+          // https://github.com/danielmahon/gatsby-remark-relative-images
+					`gatsby-remark-relative-images`,
+					{
+						resolve : `gatsby-remark-images`,
+						options : {
+							// It's important to specify the maxWidth (in pixels) of
+							// the content container as this plugin uses this as the
+							// base for generating different widths of each image.
+							maxWidth : 590
+						}
+					}
+				]
+			}
+		},
+		`gatsby-transformer-remark`,
+		`gatsby-plugin-netlify`,
+		`gatsby-plugin-sitemap`,
+		`gatsby-plugin-netlify-cms`,
+		`gatsby-plugin-styled-components`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		`gatsby-plugin-offline`,
+		{
+			resolve : `gatsby-plugin-manifest`,
+			options : {
+				name             : `William Johnson Personal Site`,
+				short_name       : `wcj.io`,
+				start_url        : `/`,
+				background_color : `#663399`,
+				theme_color      : `#663399`,
+				display          : `minimal-ui`,
+				icon             : `src/images/favicon.png` // This path is relative to the root of the site.
+			}
+		},
+		{
+			resolve : `gatsby-plugin-typography`,
+			options : {
+				pathToConfigModule : `src/utils/typography.js`
+			}
+		},
+	]
+};
