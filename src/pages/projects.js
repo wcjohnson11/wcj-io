35e4667abac367568bab94d5469c1a8680eaba13
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-13 12:45:53 -0700

    create projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Projects = ({location}) => (
+    <Layout location={location}>
+        <div>
+            <h1>Projects</h1>
+        </div>
+    </Layout>
+)
+
+export default Projects;
