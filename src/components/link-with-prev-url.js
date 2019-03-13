51c134222ee29f6d64a9fe4d22b3174070932ff5
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-13 10:46:28 -0700

    update comment

--- src/components/link-with-prev-url.js
+++ src/components/link-with-prev-url.js
@@ -1,5 +1,10 @@
 /*
-* Create Link that will always pass it's prevUrlPath to the location object
+* Create Link that will always pass it's prevUrlPath
+* into the location props via it's state
+* This is normally only accessible in Route Components
+* Uses the @reach/router's Location component to pass
+* anywhere in the app via a child render prop
+* https://reach.tech/router/api/Location
 */
 import React from 'react';
 import { Location } from '@reach/router';
