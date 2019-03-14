bd79a8a833dd7f1c4dc5d70ef0380071cbb3a131
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-14 14:38:03 -0700

    add headers for projects and blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import BlogListing from "../components/blogListing"
 import ProjectListing from "../components/projectListing"
 
+const H2 = styled.h2`
+  padding: 1rem;
+`
+
 const IndexPage = ({location}) => (
   <Layout location={location}>
+    <H2>Blog Posts</H2>
     <BlogListing />
+    <H2>Projects</H2>
     <ProjectListing />
   </Layout>
 )
