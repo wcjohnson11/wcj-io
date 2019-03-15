92d2552555a079fdc7a1d086fa48521889ee78c7
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-15 11:29:45 -0600

    create well-being project page

---
+++ src/pages/well-being.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Layout from '../components/layout.js';
+
+const WellBeing = ({location}) => (
+    <Layout location={location}>
+        <h1>Measuring Well-Being for a better World</h1>
+    </Layout>
+);
+
+export default WellBeing;
