023e76b93b9c658e6e59ea0085f09858a3696ad5
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-20 21:59:55 -0400

    rename wellBeing project page

--- src/pages/projects/wellBeing.js
+++ src/pages/projects/wellBeing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Layout from '../components/layout.js';
+import Layout from '../../components/layout.js';
 
 const WellBeing = ({location}) => (
     <Layout location={location}>
