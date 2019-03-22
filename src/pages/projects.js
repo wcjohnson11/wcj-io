c616d7714df9cbf572b73607d80ab810356a4ddc
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-21 22:42:44 -0400

    add projectListing to Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
 import Layout from '../components/layout'
+import ProjectListing from '../components/ProjectListing'
 
 const Projects = ({location}) => (
     <Layout location={location}>
         <div>
             <h1>Projects</h1>
+            <ProjectListing />
         </div>
     </Layout>
 )
