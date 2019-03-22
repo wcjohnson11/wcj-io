b11ae894d302b26c36881bab96e4ff5d21c5b2f3
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-21 22:41:33 -0400

    add blogListing to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
 import Layout from '../components/layout'
+import BlogListing from '../components/blogListing'
 
 const Blog = ({location}) => (
     <Layout location={location}>
         <div>
             <h1>Blog</h1>
+            <BlogListing />
         </div>
     </Layout>
 )
