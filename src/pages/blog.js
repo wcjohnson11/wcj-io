c9aee83ad674764f261235545400a345e4f55176
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-22 19:37:30 -0400

    update blog styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import BlogListing from '../components/blogListing'
 
 const Blog = ({location}) => (
     <Layout location={location}>
-        <div>
-            <h1>Blog</h1>
-            <BlogListing />
-        </div>
+        <h1>Blog</h1>
+        <BlogListing />
     </Layout>
 )
 
