c862fc409c920ba7e8d95ca7df09cb09cd624a75
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-28 18:41:41 -0400

    format src/pages/projects/*

--- src/pages/projects/well-being.js
+++ src/pages/projects/well-being.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import Layout from '../../components/layout.js';
 
-const WellBeing = ({location}) => (
-    <Layout location={location}>
-        <h1>Measuring Well-Being for a better World</h1>
-    </Layout>
+const WellBeing = ({ location }) => (
+	<Layout location={location}>
+		<h1>Measuring Well-Being for a better World</h1>
+	</Layout>
 );
 
 export default WellBeing;
