a7b71d9b7f552c70d58410ea38c38bc51a7f8ab1
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-31 19:16:04 -0400

    add query to well-being project page

--- src/pages/projects/well-being.js
+++ src/pages/projects/well-being.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../../components/layout.js';
 
-const WellBeing = ({ location }) => (
-	<Layout location={location}>
-		<h1>Measuring Well-Being for a better World</h1>
-	</Layout>
-);
+const WellBeing = ({ data, location }) => {
+	console.log(data)
+	const { frontmatter, html } = data.markdownRemark;
+	return (
+		<Layout location={location}>
+			<h1>{frontmatter.title}</h1>
+		</Layout>
+	)
+};
 
 export default WellBeing;
+
+export const query = graphql`
+	{
+		markdownRemark(frontmatter: { title: { regex: "/Well-Being/" } }) {
+			html
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				title
+			}
+		}
+	}
+`;
