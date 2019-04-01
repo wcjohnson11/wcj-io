4811de1c47568e163db705cace50e212ac6886e0
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-31 23:52:02 -0400

    update graphql query and add markdown div

--- src/pages/projects/well-being.js
+++ src/pages/projects/well-being.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import Layout from '../../components/layout.js';
+import MarkdownDiv from '../../components/markdownDiv';
 
 const WellBeing = ({ data, location }) => {
-	const { frontmatter } = data.markdownRemark;
-	const { sections } = frontmatter;
-	// Create dictionary of Sections for the project
-	// keys are the markdown names and values are the markdown sections
-	// { name: markdown, name: markdown... }
-	const sectionsDict = {};
-	for (var index in sections) {
-		const { name, markdown } = sections[index];
-		sectionsDict[name] = markdown;
+	const { fields, frontmatter } = data.markdownRemark;
+	const sectionHTML = {};
+	for (var field in fields) {
+		sectionHTML[field] = fields[field];
 	}
 
 	return (
 		<Layout location={location}>
 			<h1>{frontmatter.title}</h1>
 			<small>{frontmatter.date}</small>
+			<MarkdownDiv content={sectionHTML['Introduction']} />
+			<MarkdownDiv content={sectionHTML['Beyond_GDP']} />
+			<MarkdownDiv content={sectionHTML['Conclusion']} />
 		</Layout>
 	);
 };
@@ -28,6 +27,16 @@ export default WellBeing;
 export const query = graphql`
 	{
 		markdownRemark(frontmatter: { title: { regex: "/Well-Being/" } }) {
+			fields {
+				Introduction
+				Beyond_GDP
+				GINI_Index
+				Happy_Planet_Index
+				Human_Development_Index
+				Sustainable_Economic_Development_Index
+				World_Happiness_Report_Score
+				Conclusion
+			}
 			frontmatter {
 				date(formatString: "MMMM DD, YYYY")
 				title
