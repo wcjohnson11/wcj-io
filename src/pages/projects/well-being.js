c85e43c0007bde60ab7b5ff1279f74d33ab698cf
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-04-03 14:50:53 -0400

    add react hooks state and metrics selection

--- src/pages/projects/well-being.js
+++ src/pages/projects/well-being.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'gatsby';
+import Select from 'react-select';
 
 import Layout from '../../components/layout.js';
 import MarkdownDiv from '../../components/markdownDiv';
 
+// Mapping of metrics to markdownRemark keys
+const metrics = [
+	{ label: 'GINI Index', value: 'GINI_Index' },
+	{ label: 'Happy Planet Index', value: 'Happy_Planet_Index' },
+	{ label: 'Human Development Index', value: 'Human_Development_Index' },
+	{ label: 'Sustainable Economic Development Index', value: 'Sustainable_Economic_Development_Index' },
+	{ label: 'World Happiness Report Score', value: 'World_Happiness_Report_Score' }
+];
+
 const WellBeing = ({ data, location }) => {
+	// Set up component state
+	const [
+		metric,
+		setMetric
+	] = useState(metrics[0]);
+	// Extract sectionHTML from markdownRemark
 	const { fields, frontmatter } = data.markdownRemark;
 	const sectionHTML = {};
 	for (var field in fields) {
 		sectionHTML[field] = fields[field];
+	}
+
+	// handle <Select> onChange for metric
+	function handleMetricChange(metric){
+		setMetric(metric);
 	}
 
 	return (
@@ -17,6 +38,20 @@ const WellBeing = ({ data, location }) => {
 			<small>{frontmatter.date}</small>
 			<MarkdownDiv content={sectionHTML['Introduction']} />
 			<MarkdownDiv content={sectionHTML['Beyond_GDP']} />
+			<Select
+				value={{ label: metric.label, value: metric.value }}
+				onChange={handleMetricChange}
+				controlShouldRenderValue={true}
+				options={metrics.map((metric) => {
+					return {
+						label : metric.label,
+						value : metric.value
+					};
+				})}
+				blurInputOnSelect={false}
+				isMulti={false}
+			/>
+			<MarkdownDiv content={sectionHTML[metric.value]} />
 			<MarkdownDiv content={sectionHTML['Conclusion']} />
 		</Layout>
 	);
