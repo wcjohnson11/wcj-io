8d1d4bf44b13a69305da15bd2897fe4d388a8ca6
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-04-08 21:49:47 -0400

    add multiline html and line function

--- src/components/visualizations/multiline.js
+++ src/components/visualizations/multiline.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { extent, scaleLinear, scaleTime } from 'd3';
+import { extent, line, nest, scaleLinear, scaleTime } from 'd3';
 
 import DropdownSelect from '../dropdownSelect';
 
@@ -7,7 +7,13 @@ const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 const height= 500;
 const width = 700;
 const MultiLine = ({ countryOptions, data }) => {
-	const [
+	// Get list of active countries
+	const activeCountries = selected.map((d) => d.value);
+
+	// Get filtered Data for active countries
+	const filteredData = data.filter((d) => activeCountries.indexOf(d.Entity) >= 0);
+	
+    const [
 		selected,
 		setSelected
 	] = useState([
@@ -23,12 +29,6 @@ const MultiLine = ({ countryOptions, data }) => {
     const xAxisRef = useRef(null);
     const yAxisRef = useRef(null);
 
-	// Get list of active countries
-	const activeCountries = selected.map((d) => d.value);
-
-	// Get filtered Data for active countries
-	const filteredData = data.filter((d) => activeCountries.indexOf(d.Entity) >= 0);
-
     // Declare Axis
     const xAxis = axisBottom().tickOuterSize(0);
     const yAxis = axisLeft().tickOuterSize(0);
@@ -42,6 +42,12 @@ const MultiLine = ({ countryOptions, data }) => {
         .domain(extent(filteredData, d => d["GDP per capita"]))
         .nice()
         .range([height - margin.bottom, margin.top]);
+
+    // Declare line function
+    const lineFn = line()
+        .curve(curveCatMullRom)
+        .x(d => xScale(d.Year))
+        .y(d => yScale(d["GDP per capita"]))
 
     // Handle SelectionChange and update selected state
 	function handleSelectionChange(selections){
@@ -57,6 +63,25 @@ const MultiLine = ({ countryOptions, data }) => {
 				handleChange={handleSelectionChange}
 				isMulti={true}
 			/>
+            <svg className="multilineSvg" id="multiline" height={height} width={width}>
+                <g
+                    className="xAxis"
+                    ref={xAxisref}
+                    transform={`translate(0, ${height - margin.bottom})`}
+                />
+                <g><text className="xAxisTitle"></text></g>
+                <g
+                    className="yAxis"
+                    ref={yAxisRef}
+                    transform={`translate(${margin.left}, 0)`}
+                />
+                <g><text className="yAxisTitle"></text></g>
+                <g className="focus" transform={`translate(-100, -100)`}>
+                    <circle r={3.5} />
+                    <text y={-10} />
+                </g>
+                <g className="voronoi" fill="none" />
+            </svg>
 		</div>
 	);
 };
