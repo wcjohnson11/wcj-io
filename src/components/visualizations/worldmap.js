d6a4b0c7d70de81ca410ee987596a93c2221db73
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-04-23 22:51:18 -0400

    append legend scale

--- src/components/visualizations/worldmap.js
+++ src/components/visualizations/worldmap.js
@@ -1,18 +1,20 @@
-import React, { useRef } from "react";
-import { axisBottom, extent, geoEqualEarth, geoPath, interpolateOranges, scaleLinear, scaleSequential, select, selectAll, ticks } from 'd3';
+import React, { useEffect, useRef } from "react";
+import { axisBottom, extent, geoEqualEarth, geoPath, interpolateOranges, scaleLinear, scaleSequential, select, ticks } from 'd3';
 import * as topojson from 'topojson-client';
 import { withTheme } from "styled-components";
 import world from 'world-atlas/world/110m';
 
-const WorldMap = ({ data, theme, windowWidth }) => {
+import CenteringDiv from '../centeringDiv';
+
+const WorldMap = ({ data, metric, theme, windowWidth }) => {
   // Define Refs for D3
   const svgRef = useRef(null);
   const legendRef = useRef(null);
   const gradientRef = useRef(null);
 
   // Define dimensions of map on window width
-  const width = windowWidth;
-  const height = windowWidth * 0.75;
+  const width = windowWidth > 796 ? 796 : windowWidth - 90;
+  const height = width * 0.75;
 
   // Create topology country objects
   const topoCountries = topojson.feature(world, world.objects.countries).features;
@@ -20,19 +22,126 @@ const WorldMap = ({ data, theme, windowWidth }) => {
   // Create topology mesh objects
   const topoMesh = topojson.mesh(world, world.objects.countries, (a, b) => a !== b);
 
+  // Declare metric extent on countries that have values
+  const metricExtent = extent(data, d => {
+    if (d[metric.value] !== "FALSE") {
+      return d[metric.value]
+    }
+  });
+
   // Define Scales
-  // const colorScale = scaleSequential(interpolateOranges).domain(extent)
+  const colorScale = scaleSequential(interpolateOranges).domain(metricExtent);
+  const xScale = scaleLinear()
+    .domain(extent(colorScale.domain()))
+    .rangeRound([width / 2 - 120, width / 2 + 120]);
+  
+  // Declare Legend Ticks
+  const legendTicks = ticks(metricExtent[0], metricExtent[1], 4);
+
+  // Declare Legend Axis
+  const legendAxis = axisBottom(xScale)
+    .tickSize(13)
+    .tickValues(legendTicks);
+
+  // Declare Projection
+  const projection = geoEqualEarth()
+    .rotate([-10, 0])
+    .fitExtent([[1,1], [width - 1, height - 51]], {type: "Sphere"})
+    .precision(0.1);
+
+  // Declare Geography Paths
+  const geographyPaths = geoPath()
+    .projection(projection);
+
+  function getCountryColor(id, data, color, metric) {
+    const matchedCountry = data.find(country => country.code === id);
+
+    if (matchedCountry && matchedCountry[metric]) {
+      return color(matchedCountry[metric]);
+    } else {
+      return theme.colorBorder;
+    }
+  };
+
+  function getCountryLabel(id, data) {
+    const text = data.find(country => country.code === id);
+    if (text) return text.name;
+    return "Unknown"
+  };
+
+  useEffect(() => {
+    // Append legend ticks
+    select(legendRef.current)
+      .call(legendAxis)
+      .select(".domain")
+      .remove();
+
+    // Add colors to gradient
+     select("#linear-gradient")
+       .selectAll("stop")
+       .data(
+         legendTicks.map((t, i, n) => ({
+           offset: `${(100 * i) / n.length}%`,
+           color: colorScale(t)
+         }))
+       )
+       .enter()
+       .append("stop")
+       .attr("offset", d => d.offset)
+       .attr("stop-color", d => d.color);
+
+    // Add metric label to caption
+    select('.caption').text(`${metric.label} scale`);
+
+    // Create border
+    select(svgRef.current)
+      .append("path")
+      .datum({ type: "Sphere" })
+      .attr("fill", theme.colorPrimary)
+      .attr("fill-rule", "nonzero")
+      .attr("stroke", theme.colorBorder)
+      .attr("stroke-linejoin", "round")
+      .attr("d", geographyPaths);
+
+    // Add country features
+    const features = select(svgRef.current)
+         .append("g")
+         .selectAll("path")
+         .data(topoCountries)
+         .enter()
+         .append("path")
+         .attr("fill", 'red')
+         .attr("d", geographyPaths);
+
+  }, [])
 
   return (
-    <svg height={height} width={width} ref={svgRef}>
-      <defs>
-        <linearGradient id="linear-gradient" ref={gradientRef} />
-      </defs>
-      <g ref={legendRef} transform={`translate(0, ${height - width / 9})`}>
-        <rect />
-        <text className="caption" fill={theme.colorPrimary} />
-      </g>
-    </svg>
+    <CenteringDiv>
+      <svg height={height} width={width} ref={svgRef}>
+        <defs>
+          <linearGradient id="linear-gradient" ref={gradientRef} />
+        </defs>
+        <g ref={legendRef} transform={`translate(0, ${height - width / 9})`}>
+          <rect
+            height="8"
+            width={
+              xScale(legendTicks[legendTicks.length - 1]) -
+              xScale(legendTicks[0])
+            }
+            x={xScale(legendTicks[0])}
+            fill={`url(#linear-gradient)`}
+          />
+          <text
+            className="caption"
+            x={xScale(legendTicks[0])}
+            y={-6}
+            fill={theme.colorBase}
+            textAnchor="start"
+            fontWeight={theme.fontBold}
+          />
+        </g>
+      </svg>
+    </CenteringDiv>
   );
 };
 
--- src/pages/projects/well-being.js
+++ src/pages/projects/well-being.js
@@ -1,132 +1,138 @@
-import React, { useState } from 'react';
-import { graphql } from 'gatsby';
-import { set, timeParse } from 'd3';
-import  useWindowSize from '../../utils/useWindowSize';
+import React, { useState } from "react";
+import { graphql } from "gatsby";
+import { set, timeParse } from "d3";
+import useWindowSize from "../../utils/useWindowSize";
 
-import Layout from '../../components/layout';
-import MarkdownDiv from '../../components/markdownDiv';
-import MultiLine from '../../components/visualizations/multiline';
-import Heatmap from '../../components/visualizations/heatmap';
-import DropdownSelect from '../../components/dropdownSelect';
-import WorldMap from '../../components/visualizations/WorldMap';
+import Layout from "../../components/layout";
+import MarkdownDiv from "../../components/markdownDiv";
+import MultiLine from "../../components/visualizations/multiline";
+import Heatmap from "../../components/visualizations/heatmap";
+import DropdownSelect from "../../components/dropdownSelect";
+import WorldMap from "../../components/visualizations/WorldMap";
 
 // Mapping of metrics to markdownRemark keys
 const metrics = [
-	{ label: 'GINI Index', value: 'GINI_Index' },
-	{ label: 'Happy Planet Index', value: 'Happy_Planet_Index' },
-	{ label: 'Human Development Index', value: 'Human_Development_Index' },
-	{ label: 'Sustainable Economic Development Index', value: 'Sustainable_Economic_Development_Index' },
-	{ label: 'World Happiness Report Score', value: 'World_Happiness_Report_Score' }
+  { label: "GINI Index", value: "GINI_Index" },
+  { label: "Happy Planet Index", value: "Happy_Planet_Index" },
+  { label: "Human Development Index", value: "Human_Development_Index" },
+  {
+    label: "Sustainable Economic Development Index",
+    value: "Sustainable_Economic_Development_Index"
+  },
+  {
+    label: "World Happiness Report Score",
+    value: "World_Happiness_Report_Score"
+  }
 ];
 
-const cleanNumbers = (string) => {
-	return parseFloat(string.replace(/,/g, ''));
+const cleanNumbers = string => {
+  return parseFloat(string.replace(/,/g, ""));
 };
-const parseTime = timeParse('%Y');
+const parseTime = timeParse("%Y");
 
 const WellBeing = ({ data, location }) => {
-	// Set up component state
-	const [
-		metric,
-		setMetric
-	] = useState(metrics[0]);
+  // Set up component state
+  const [metric, setMetric] = useState(metrics[0]);
 
-	// Get window size for responsive charts
-	const windowSize = useWindowSize();
+  // Get window size for responsive charts
+  const windowSize = useWindowSize();
 
-	// Extract sectionHTML from markdownRemark
-	const { fields, frontmatter } = data.markdownRemark;
-	const sectionHTML = {};
-	for (var field in fields) {
-		sectionHTML[field] = fields[field];
-	}
+  // Extract sectionHTML from markdownRemark
+  const { fields, frontmatter } = data.markdownRemark;
+  const sectionHTML = {};
+  for (var field in fields) {
+    sectionHTML[field] = fields[field];
+  }
 
-	// Extract gdp data from allGdpovertimeCsv for multiline chart
-	const { edges } = data.allGdpovertimeCsv;
-	const multilineNodes = edges.map(({ node }) => {
-		return {
-			Entity: node.Entity,
-			Code: node.Code,
-			Year: parseTime(node.Year),
-			'GDP per capita': cleanNumbers(node['GDP_per_capita'])
-		};
-	});
-	// Get list of countries from allGdpovertimeCSV for multiline dropdown
-	const countryOptions = set(multilineNodes.map(d => d.Entity)).values();
+  // Extract gdp data from allGdpovertimeCsv for multiline chart
+  const { edges } = data.allGdpovertimeCsv;
+  const multilineNodes = edges.map(({ node }) => {
+    return {
+      Entity: node.Entity,
+      Code: node.Code,
+      Year: parseTime(node.Year),
+      "GDP per capita": cleanNumbers(node["GDP_per_capita"])
+    };
+  });
+  // Get list of countries from allGdpovertimeCSV for multiline dropdown
+  const countryOptions = set(multilineNodes.map(d => d.Entity)).values();
 
+  // Get Happiness data from allHappy2CSV
+  const happinessData = data.allHappy2Csv.nodes;
 
-	// Get Happiness data from allHappy2CSV
-	const happinessData = data.allHappy2Csv.nodes;
+  // handle <Select> onChange for metric
+  function handleMetricChange(metric) {
+    setMetric(metric);
+  }
 
-	// handle <Select> onChange for metric
-	function handleMetricChange(metric){
-		setMetric(metric);
-	}
-
-	return (
-		<Layout location={location}>
-			<h1>{frontmatter.title}</h1>
-			<small>{frontmatter.date}</small>
-			<MarkdownDiv content={sectionHTML['Introduction']} />
-			<MultiLine countryOptions={countryOptions} data={multilineNodes} windowWidth={windowSize.width} />
-			<MarkdownDiv content={sectionHTML['Beyond_GDP']} />
-			<Heatmap metrics={metrics} />
-			<DropdownSelect
-				currentSelection={metric}
-				options={metrics}
-				handleChange={handleMetricChange}
-				isMulti={false}
-			/>
-			<MarkdownDiv content={sectionHTML[metric.value]} />
-			<WorldMap data={happinessData} windowWidth={windowSize.width}/>
-			<MarkdownDiv content={sectionHTML['Conclusion']} />
-		</Layout>
-	);
+  return (
+    <Layout location={location}>
+      <h1>{frontmatter.title}</h1>
+      <small>{frontmatter.date}</small>
+      <MarkdownDiv content={sectionHTML["Introduction"]} />
+      <MultiLine
+        countryOptions={countryOptions}
+        data={multilineNodes}
+        windowWidth={windowSize.width}
+      />
+      <MarkdownDiv content={sectionHTML["Beyond_GDP"]} />
+      <Heatmap metrics={metrics} />
+      <DropdownSelect
+        currentSelection={metric}
+        options={metrics}
+        handleChange={handleMetricChange}
+        isMulti={false}
+      />
+      <MarkdownDiv content={sectionHTML[metric.value]} />
+      <WorldMap data={happinessData} metric={metric} windowWidth={windowSize.width} />
+      <MarkdownDiv content={sectionHTML["Conclusion"]} />
+    </Layout>
+  );
 };
 
 export default WellBeing;
 
 export const query = graphql`
-	{
-		markdownRemark(frontmatter: { title: { regex: "/Well-Being/" } }) {
-			fields {
-				Introduction
-				Beyond_GDP
-				GINI_Index
-				Happy_Planet_Index
-				Human_Development_Index
-				Sustainable_Economic_Development_Index
-				World_Happiness_Report_Score
-				Conclusion
-			}
-			frontmatter {
-				date(formatString: "MMMM DD, YYYY")
-				title
-				sections {
-					markdown
-					name
-				}
-			}
-		}
-		allGdpovertimeCsv {
-			edges {
-				node {
-					Entity
-					Code
-					Year
-					GDP_per_capita
-				}
-			}
-		}
-		allHappy2Csv(skip: 9) {
-    nodes {
-      name
-      GINI_Index
-      World_Happiness_Report_Score
-      Sustainable_Economic_Development_Index
-      Happy_Planet_Index
-      Human_Development_Index
+  {
+    markdownRemark(frontmatter: { title: { regex: "/Well-Being/" } }) {
+      fields {
+        Introduction
+        Beyond_GDP
+        GINI_Index
+        Happy_Planet_Index
+        Human_Development_Index
+        Sustainable_Economic_Development_Index
+        World_Happiness_Report_Score
+        Conclusion
+      }
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        sections {
+          markdown
+          name
+        }
+      }
+    }
+    allGdpovertimeCsv {
+      edges {
+        node {
+          Entity
+          Code
+          Year
+          GDP_per_capita
+        }
+      }
+    }
+    allHappy2Csv(skip: 9) {
+      nodes {
+        name
+        GINI_Index
+        World_Happiness_Report_Score
+        Sustainable_Economic_Development_Index
+        Happy_Planet_Index
+        Human_Development_Index
+      }
     }
   }
-	}
 `;
