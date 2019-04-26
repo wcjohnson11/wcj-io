753830ccab20b107ee8603ef67e6962f7cce9e79
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-04-25 20:45:03 -0400

    create glmap component

---
+++ src/components/visualizations/glmap.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import MapGL from "react-map-gl";
+import * as topojson from "topojson-client";
+
+import GLChlorpleth from "./glchloropleth";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import ChoroplethOverlay from "./glchloropleth";
+
+const GLMap = ({ data }) => {
+  const [viewport, setViewport] = useState({
+    height: 700,
+    width: 700,
+    latitude: 30.375999878729193,
+    longitude: 11.258032299596003,
+    zoom: 0.75,
+    bearing: 0,
+    pitch: 0
+  });
+
+  // Create topology country objects
+  const topoCountries = topojson.feature(world, world.objects.countries)
+    .features;
+
+  // Value accessor function
+  function getValue (feature) {
+      console.log(feature)
+  }
+
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%"
+      }}
+    >
+      <MapGL
+        {...viewport}
+        mapStyle={"mapbox://styles/wcj111/cjuvm4fbw19bp1fphhnuw1vel"}
+        mapboxApiAccessToken={process.env.MapboxAccessToken}
+        onViewportChange={viewport => setViewport(viewport)}
+      >
+        <ChoroplethOverlay
+          features={topoCountries}
+          colorDomain={[0, 100]}
+          colorRange={["hsl(0.8, 1, 0.8)", "hsl(0, 1, 1)"]}
+          valueAccessor={getValue(feature)}
+          globalOpacity={0.8}
+          renderWhileDragging={true}
+        />
+      </MapGL>
+    </div>
+  );
+};
+
+export default GLMap;
