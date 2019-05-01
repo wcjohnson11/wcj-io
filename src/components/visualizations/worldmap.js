b95c41e3ad0f26ab59eedfc663d8aae75b65437c
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-05-01 15:34:04 -0400

    set useEffect to be triggered off metric change

--- src/components/visualizations/worldmap.js
+++ src/components/visualizations/worldmap.js
@@ -140,7 +140,7 @@ const WorldMap = ({ data, metric, theme, windowWidth }) => {
       .attr("stroke-width", "0.5px")
       .attr("stroke-linejoin", "round")
       .attr("d", geographyPaths);
-  }, []);
+  }, [metric]);
 
   return (
     <CenteringDiv>
@@ -157,7 +157,7 @@ const WorldMap = ({ data, metric, theme, windowWidth }) => {
               d="M-1,1 l2,-2
            M0,4 l4,-4
            M3,5 l2,-2"
-              style={{stroke: "black", strokeWidth:1 }}
+              style={{ stroke: "black", strokeWidth: 1 }}
             />
           </pattern>
         </defs>
